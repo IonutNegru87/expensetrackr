4dd78d47215b805ec204ae570cf4e23ca4863a06
Ionut Negru <ionut.negru87@gmail.com>
2024-07-10 21:44:12 +0300

    Add button and logic for taking photo

--- app/src/main/java/com/inegru/expensetrackr/ui/screens/AddExpenseScreen.kt
+++ app/src/main/java/com/inegru/expensetrackr/ui/screens/AddExpenseScreen.kt
@@ -1,5 +1,8 @@
 package com.inegru.expensetrackr.ui.screens
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,14 +19,39 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.FileProvider
 import androidx.navigation.NavHostController
+import com.inegru.expensetrackr.ext.createImageFile
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddExpenseScreen(navController: NavHostController) {
+
+    var photoUri by remember { mutableStateOf<Uri?>(null) }
+
+    val file = LocalContext.current.createImageFile()
+    val uri = FileProvider.getUriForFile(
+        LocalContext.current,
+        "${LocalContext.current.packageName}.fileprovider",
+        file
+    )
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicture(),
+        onResult = { success ->
+            if (success) {
+                photoUri = uri
+            }
+        }
+    )
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -44,13 +72,10 @@ fun AddExpenseScreen(navController: NavHostController) {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Text("Add Expense Screen")
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(
-                onClick = { navController.navigateUp() }
-            ) {
-                Text("Save Expense")
+            Button(onClick = { launcher.launch(uri) }) {
+                Text("Take Photo")
             }
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
