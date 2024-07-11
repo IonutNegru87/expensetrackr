4447ebca9b89b460a6377be23bfe6ee8e0353fb8
Ionut Negru <ionut.negru87@gmail.com>
2024-07-11 20:52:26 +0300

    Add support for Total details

--- app/src/main/java/com/inegru/expensetrackr/ui/screens/AddExpenseScreen.kt
+++ app/src/main/java/com/inegru/expensetrackr/ui/screens/AddExpenseScreen.kt
@@ -11,12 +11,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -28,6 +32,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.core.content.FileProvider
 import androidx.navigation.NavHostController
@@ -41,6 +47,9 @@ fun AddExpenseScreen(navController: NavHostController) {
 
     var photoUri by remember { mutableStateOf<Uri?>(null) }
     var date by remember { mutableStateOf("") }
+    var total by remember { mutableStateOf("") }
+
+    var totalError by remember { mutableStateOf<String?>(null) }
 
     val file = LocalContext.current.createImageFile()
     val uri = FileProvider.getUriForFile(
@@ -56,6 +65,8 @@ fun AddExpenseScreen(navController: NavHostController) {
             }
         }
     )
+
+    val focusManager = LocalFocusManager.current
 
     Scaffold(
         topBar = {
@@ -90,15 +101,47 @@ fun AddExpenseScreen(navController: NavHostController) {
                         .fillMaxWidth(0.5f)
                         .padding(8.dp)
                 )
+                Spacer(modifier = Modifier.height(8.dp))
             }
 
-            Spacer(modifier = Modifier.height(8.dp))
             OutlinedDatePickerField(
                 date = date,
                 onDateChange = { date = it },
                 modifier = Modifier.fillMaxWidth()
             )
             Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = total,
+                onValueChange = {
+                    total = it
+                    totalError = validateTotal(it)
+                },
+                label = { Text("Total") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+                keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus(true)}),
+                modifier = Modifier.fillMaxWidth(),
+                isError = totalError != null,
+                supportingText = {
+                    totalError?.let {
+                        Text(
+                            text = it,
+                            color = MaterialTheme.colorScheme.error
+                        )
+                    }
+                },
+                singleLine = true,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
         }
+    }
+}
+
+private fun validateTotal(total: String): String? {
+    return when {
+        total.isEmpty() -> "Total is required"
+        total.toDoubleOrNull() == null -> "Invalid number format"
+        total.toDouble() <= 0 -> "Total must be greater than zero"
+        else -> null
     }
 }
